9188e651afd7c0f6b4eafa04705d652fa82c9337
Gregory Higley <gregory.higley@patrontechnology.com>
2023-06-28 15:36:17 -0400

    Inject environment variables

--- main.go
+++ main.go
@@ -30,26 +30,29 @@ func main() {
 		os.Exit(1)
 	}
 
-	logger.Infof("Apple ID: %s", connection.AppleIDConnection.AppleID)
-
-	//	fmt.Println("This is the value specified for the input 'example_step_input':", os.Getenv("example_step_input"))
-	//
-	// --- Step Outputs: Export Environment Variables for other Steps:
-	// You can export Environment Variables for other Steps with
-	//  envman, which is automatically installed by `bitrise setup`.
-	// A very simple example:
-	cmdLog, err := exec.Command("bitrise", "envman", "add", "--key", "EXAMPLE_STEP_OUTPUT", "--value", "the value you want to share").CombinedOutput()
+	session, err := connection.AppleIDConnection.FastlaneLoginSession()
 	if err != nil {
-		fmt.Printf("Failed to expose output with envman, error: %#v | output: %s", err, cmdLog)
+		logger.Errorf("Failed to get Fastlane session: %s", err)
 		os.Exit(1)
 	}
-	// You can find more usage examples on envman's GitHub page
-	//  at: https://github.com/bitrise-io/envman
 
-	//
-	// --- Exit codes:
-	// The exit code of your Step is very important. If you return
-	//  with a 0 exit code `bitrise` will register your Step as "successful".
-	// Any non zero exit code will be registered as "failed" by `bitrise`.
+	environmentVariables := map[string]string{
+		"FASTLANE_USER":     connection.AppleIDConnection.AppleID,
+		"FASTLANE_PASSWORD": connection.AppleIDConnection.Password,
+		"FASTLANE_SESSION":  session,
+	}
+	setEnv(environmentVariables)
 	os.Exit(0)
+}
+
+func setEnv(vars map[string]string) {
+	for key, value := range vars {
+		// You can find more usage examples on envman's GitHub page
+		//  at: https://github.com/bitrise-io/envman
+		cmdLog, err := exec.Command("bitrise", "envman", "add", "--key", key, "--value", value).CombinedOutput()
+		if err != nil {
+			fmt.Printf("Failed to expose output with envman, error: %#v | output: %s", err, cmdLog)
+			os.Exit(1)
+		}
+	}
 }
